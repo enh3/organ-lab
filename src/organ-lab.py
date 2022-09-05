5eeb0fb0b319c78c92e929fb81d01431c16cdd9f
enh3 <xkjelx@gmail.com>
2022-09-05 16:34:37 -0400

    2022-08-28 (Test2)

--- src/organ-lab.py
+++ src/organ-lab.py
@@ -5,32 +5,28 @@ s = Server()
 s.setMidiInputDevice(99)
 s.boot()
 partials = list(range(1, 8, 1))
-mul = [(0.3**i)*0.0625 for i in range(7)]
-#mul[0], mul[1] = mul[1], mul[0]
-print(mul)
+mul = [(0.4**i)*0.0625 for i in range(7)]
 note = Notein(poly=10, scale=0, first=0, last=127, channel=0, mul=1)
 
 note.keyboard()
 
 env = MidiAdsr(note['velocity'], attack=0.003, decay=0.1, sustain=0.7, release=0.5)
 noiseEnv = MidiAdsr(note['velocity'], attack=0.001, decay=0.01, sustain=0.1, release=0.1)
-p1Env = MidiAdsr(note['velocity'], attack=0.001, decay=0.01, sustain=0.1, release=0.1)
-p2Env = MidiAdsr(note['velocity'], attack=0.01, decay=0.01, sustain=0.2, release=0.1)
 
-freq = MToF(note['pitch'])
 
-#amps = Port(note["velocity"], risetime=0.005, falltime=0.5, mul=0.1)
+freq = MToF(note["pitch"])
+
+
+amps = Port(note["velocity"], risetime=0.005, falltime=0.5, mul=0.1)
 
 pitch = [(partial * freq) for partial in partials]
 
 #lfMul = [(1/i)*10 for i in freq]
 #lf = Sine(0.5, mul=lfMul)
 
-noise = PinkNoise(0.7) * noiseEnv
-noise = Reson(noise, freq=freq*4, q=4)
-sound = [Sine(freq=pit+Randi(-2.0, 2.0, 5), mul=*[p1Env, p2Env, p1Env, p2Env, p1Env, p2Env, p1Env, p2Env]) for pit, amp in zip(pitch, mul)]
-sound = STRev(Mix(sound, 1) + noise, inpos=0.5, revtime=5, cutoff=4000, bal=0.15)
-
+sound = [Sine(freq=pit+Randi(-2.0, 2.0, 5), mul=amp*env) + BrownNoise(0.02) * noiseEnv for pit, amp in zip(pitch, mul)]
+sound = STRev(Mix(sound, 1), inpos=0.5, revtime=5, cutoff=4000, bal=0.15)
+#sound = ButLP(sound, filtEnv)
 
 SL = sound.out()
 SR = sound.out(1)
