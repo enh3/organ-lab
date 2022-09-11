0e9e2d99b232887bb84d0100a4c4f38d2f4b4c7e
enh3 <xkjelx@gmail.com>
2022-09-11 19:09:50 -0400

    2022-09-09 (Happy accident)

--- src/organ-lab.py
+++ src/organ-lab.py
@@ -9,7 +9,7 @@ t.view()
 t.graph()
 partialsInit = [1]
 mul = [1]
-randDev = 30.0
+randDev = Sig(1)
 note = Notein(poly=10, scale=0, first=0, last=127, channel=0, mul=1)
 note.keyboard()
 
@@ -28,7 +28,7 @@ noise = Reson(noise, freq=(freq*(20/4)), q=10)
 
 partEnvs = [p1Env, p2Env, p3Env, p4Env]
 
-sound = [Osc(t, freq=pit+Randi(-randDev, randDev, 5), mul=amp) for pit, amp in zip(pitch, partEnvs)]
+sound = [Osc(t, freq=pit+Randi(-randDev, randDev, 5), mul=amp*partEnvs) for pit, amp in zip(pitch, mul)]
 sound = STRev(Mix(sound, 1) + noise, inpos=0.5, revtime=5, cutoff=4000, bal=0.15)
 sound = ButLP(sound, freq=1650)
 
@@ -38,11 +38,11 @@ noiseEnv.ctrl(title="Noise Envelope")
 
 def new():
     x = random.uniform(2.0, 3.0)
-    randDev = x
-    print(randDev)
+    return(x)
 
 
-pat = Pattern(new, 10.2).play()
+randDev = Pattern(new, 2.2).play()
+
 
 SL = sound.out()
 SR = sound.out(1)
