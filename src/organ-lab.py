3b5450bca5abd52d841ade2945e18217d1ebb0c1
enh3 <xkjelx@gmail.com>
2022-09-12 15:14:48 -0400

    2022-09-11

--- src/organ-lab.py
+++ src/organ-lab.py
@@ -3,10 +3,8 @@ from pyo import *
 s = Server()
 s.setMidiInputDevice(99)
 s.boot()
-t = HarmTable([1]+[0.07]+[0.01]+[0.003]+[0]*20, size=512)
-t.autoNormalize(True)
-t.view()
-t.graph()
+
+
 partialsInit = [1]
 mul = [1]
 randDev = Sig(1)
@@ -15,10 +13,16 @@ note.keyboard()
 
 env = MidiAdsr(note['velocity'], attack=0.03, decay=0.1, sustain=0.8, release=0.5)
 noiseEnv = MidiAdsr(note['velocity'], attack=0.001, decay=0.146, sustain=0.158, release=0.1)
+print(s.getNumberOfStreams())
 p1Env = MidiAdsr(note['velocity'], attack=0.1, decay=0.01, sustain=0.1, release=0.1)
 p2Env = MidiAdsr(note['velocity'], attack=5, decay=0.01, sustain=0.2, release=0.1)
 p3Env = MidiAdsr(note['velocity'], attack=5, decay=0.01, sustain=0.2, release=0.1)
 p4Env = MidiAdsr(note['velocity'], attack=5, decay=0.01, sustain=0.2, release=0.1)
+t = HarmTable([1]+[0.07]+[0.01]+[0.003]+[0]*20, size=512)
+t.autoNormalize(True)
+t.view()
+t.graph()
+
 
 freq = MToF(note['pitch'])
 pitch = [(partial * freq) for partial in partialsInit]
@@ -28,28 +32,37 @@ noise = Reson(noise, freq=(freq*(20/4)), q=10)
 
 partEnvs = [p1Env, p2Env, p3Env, p4Env]
 
-sound = [Osc(t, freq=pit+Randi(-randDev, randDev, 5), mul=amp*partEnvs) for pit, amp in zip(pitch, mul)]
+sound = [Osc(t, freq=pit+Randi(-randDev, randDev, 5), mul=amp*env) for pit, amp in zip(pitch, mul)]
+
 sound = STRev(Mix(sound, 1) + noise, inpos=0.5, revtime=5, cutoff=4000, bal=0.15)
 sound = ButLP(sound, freq=1650)
+
 
 noise.ctrl(title="Noise")
 env.ctrl(title="Harmonics Envelope")
 noiseEnv.ctrl(title="Noise Envelope")
 
 def new():
-    x = random.uniform(2.0, 3.0)
-    return(x)
+    randDev.value = Adsr(attack=30, release=10, mul=28, add=2).play()
+    
+"""
+< 30:
+        randDev.value += 1
+    else:
+        randDev.value -= 1
+    return(randDev)
+"""
 
+randDevEnv = Pattern(new, 30).play()
 
-randDev = Pattern(new, 2.2).play()
-
+#pp = Print(randDev, interval=0.1, message="Audio stream value")
 
 SL = sound.out()
 SR = sound.out(1)
 
 print(randDev)
 
-
+s.noteout(pitch=1, velocity=20)
 
 s.start()
 s.gui(locals())
