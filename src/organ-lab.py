009cc84d11b75858f37da1685447f80297bd7ea0
enh3 <xkjelx@gmail.com>
2022-12-31 13:02:04 -0500

    2022-12-31

--- src/organ-lab.py
+++ src/organ-lab.py
@@ -12,12 +12,18 @@ s.setMidiInputDevice(0)
 s.boot()
 
 class Stop:
-    def __init__(self, part, partScale, mul, att, rel, noiseAtt, noiseRel, rand, trans, ramp):
+    def __init__(self, part, partScale, mul, att, rel, noiseAtt, noiseDec, noiseSus, noiseRel, noiseMul, noiseFiltQ, rand, trans, ramp, fmMul):
         # scale=1 to get pitch values in hertz
         self.note = Notein(poly=10, scale=1, first=0, last=127, channel=0)
         self.note.keyboard()
         self.ramp = Sig(ramp)
-        print(partScale)
+        self.fmMul = Sig(fmMul)
+        self.noiseAtt = Sig(noiseAtt)
+        self.noiseDec = Sig(noiseDec)
+        self.noiseSus = Sig(noiseSus)
+        self.noiseRel = Sig(noiseRel)
+        self.noiseMul = Sig(noiseMul)
+        self.noiseFiltQ = Sig(noiseFiltQ)
         self.amps = []
         self.envs = []
         self.part = []
@@ -25,15 +31,15 @@ class Stop:
         self.mixed = []
         self.trans = []
         self.velocity = [Clip(Sig(v), max=0.01, mul=100) for v in self.note['velocity']]
-        self.partScale = MidiAdsr(self.velocity, attack=0.001, decay=5, sustain=1/partScale, release=3, mul=partScale)#+Sig(partScale)
-        self.noiseEnv = MidiAdsr(self.note['velocity'], attack=0.001, decay=0.1, sustain=0.01, release=0.1)
+        self.partScale = MidiAdsr(self.velocity, attack=0.001, decay=5, sustain=1/partScale, release=0, mul=partScale)
+        self.noiseEnv = MidiAdsr(self.note['velocity'], attack=self.noiseAtt.value, decay=self.noiseDec.value, sustain=self.noiseSus.value, release=self.noiseRel.value)
         self.noise = PinkNoise(1.5) * self.noiseEnv
-        self.noise = Reson(self.noise, freq=(self.note['pitch']*(20/4)), q=4, mul=1)
-        self.noise = Mix(self.noise, 1)
+        self.noise = Reson(self.noise, freq=(self.note['pitch']*(20/4)), q=self.noiseFiltQ)
+        self.noise = Mix(self.noise, 1, mul=self.noiseMul)
         self.fmEnv = MidiAdsr(self.note['velocity'], attack=0.001, decay=2, sustain=0.30, release=2)
         self.fm1 = FM(carrier=self.note['pitch']*4, ratio=0.43982735, index=2.11232, mul=self.fmEnv)
         self.fm2 = FM(carrier=self.fm1, ratio=0.72348, index=1.376, mul=self.fmEnv)
-        self.fmMix = Mix(self.fm1, 2)
+        self.fmMix = Mix(self.fm1, 2, mul=self.fmMul)
         #self.pp = Print(self.partScale, interval=0.3, message="Audio stream value")
         # Handles the user polyphony independently to avoid mixed polyphony concerns (self.note already contains 10 streams)
         for i in range(len(part)):
@@ -44,9 +50,9 @@ class Stop:
             self.part.append(SigTo(part[i], time=0.2))
             self.snds.append(Sine(freq=(self.part[i]**self.partScale) * self.note['pitch'] + Randi(-rand, rand, 5) + self.trans[-1], mul=self.envs[-1]))
             self.mixed.append(self.snds[-1].mix())
-        self.mix = Mix(self.mixed, 2)
+        self.mix = Mix(self.mixed, 2, mul=1)
         self.sp = Spectrum(self.mix)
-        self.filt = EQ(self.mix+self.noise+self.fmMix, 2000, boost=0, type=2)
+        self.filt = ButLP(self.mix+self.noise+self.fmMix, 2000)
         self.rev = STRev(self.filt, inpos=0.5, revtime=5, cutoff=4000, bal=0.15)
         #self.tf = TrigFunc(self.note["trigon"], function=self.setPartScale, arg=list(range(10))) # Notein.poly defaults to 10
 
@@ -95,7 +101,27 @@ class Stop:
             
     def vel(self):
         return self.note['velocity']
-       
+    
+    def setFmMul(self, x):
+        self.fmMul.value = x
+        
+    def setNoiseAtt(self, x):
+        self.noiseAtt.value = x
+        
+    def setNoiseDec(self, x):
+        self.noiseDec.value = x
+        
+    def setNoiseSus(self, x):
+        self.noiseSus.value = x
+        
+    def setNoiseRel(self, x):
+        self.noiseRel.value = x
+    
+    def setNoiseMul(self, x):
+        self.noiseMul.value = x
+        
+    def setNoiseFiltQ(self, x):
+        self.noiseFiltQ.value = x
 
 def bourdon():
     stop1.setMul([1, 0.01, 0.1, 0.01, 0.07, 0, 0.02, 0, 0.01, 0, 0.003, 0, 0.003, 0, 0.001, 0, 0.001, 0, 0.001, 0])
@@ -178,7 +204,19 @@ def dissocie(x):
             print("setnon0")
     if dissCount == 4:
         dissCount = 0
-
+        
+def bell():
+    stop1.setFmMul(1)
+    stop1.setNoiseAtt(0.001)
+    stop1.setNoiseDec(0.1)
+    stop1.setNoiseSus(0.01)
+    stop1.setNoiseRel(0.1)    
+    stop1.setNoiseMul(1)
+    stop1.setNoiseFiltQ(4)
+    stop1.setEnvDec([4, 5, 3, .1, .3, 0.4, .04, 0.4, .4, 0.4, .4, 0.4, .4, 0.4, .4, 0.4, .4, 0.4, .4, 0.4])
+    stop1.setEnvSus([.2, .1, .2, .1, .01, 0.1, .01, 0.1, .01, 0.1, .01, 0.1, .01, 0.1, .01, 0.1, .01, 0.1, .2, 0.2])
+    stop1.setEnvRel([4]*20)
+    stop1.setPartScale(1.1)
 
 partList = list(range(1, 21, 1))
 transList = list(range(1, 21, 1))
@@ -194,16 +232,14 @@ def stateChanges(address, *args):
         i -= 1
         print(i)
     if i == 1:
+        glissUpP.play()
         #voixHumaine()
         #randPartP.play()
-        print('partielsAléatoire')
+        print('glissUp')
     elif i == 2:
-        #glissUpP.stop()
+        glissUpP.stop()
         randPartP.stop()
-        stop1.setEnvDec([4, 5, 3, .1, .3, 0.4, .04, 0.4, .4, 0.4, .4, 0.4, .4, 0.4, .4, 0.4, .4, 0.4, .4, 0.4])
-        stop1.setEnvSus([.2, .1, .2, .1, .01, 0.1, .01, 0.1, .01, 0.1, .01, 0.1, .01, 0.1, .01, 0.1, .01, 0.1, .2, 0.2])
-        stop1.setEnvRel([4]*20)
-        stop1.setPartScale(1.1)
+        
         print('scalaireDesPartiels')
     elif i == 3:
         stop1.setPartScale(1)
@@ -236,7 +272,7 @@ def stateChanges(address, *args):
 
 scan = OscDataReceive(port=9002, address="*", function=stateChanges)
 
-stop1 = Stop(partList, 1, [1, 0.01, 0.1, 0.01, 0.07, 0, 0.02, 0, 0.01, 0, 0.003, 0, 0.003, 0, 0.001, 0, 0.001, 0, 0.001, 0], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], 0.1, 0.1, 2, transList, 0.02).out()
+stop1 = Stop(partList, 1, [1, 0.01, 0.1, 0.01, 0.07, 0, 0.02, 0, 0.01, 0, 0.003, 0, 0.003, 0, 0.001, 0, 0.001, 0, 0.001, 0], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], 0.001, 0.146, 0.70, 0.1, 0.4, 10, 2, transList, 0.02, 0).out()
 
 stopV = stop1.vel()
 dummy = Sig(0)
@@ -256,7 +292,7 @@ pitch = Phasor(freq=11, mul=48, add=36)
 # Global variable to count the down and up beats.
 count = 0
 
-
+"""
 def midi_event():
     global count
     # Retrieve the value of the pitch audio stream and convert it to an int.
@@ -280,9 +316,9 @@ def midi_event():
     # and the correponding noteoff event after `duration` milliseconds.
     s.makenote(pitch=pit, velocity=vel, duration=dur)
 
-
 # Generates a MIDI event every 125 milliseconds.
 pat = Pattern(midi_event, 0.5).play()
+"""
 
 s.amp = 0.3
 
