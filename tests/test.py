cf41e74b429d6c73bbeaec9cee4064019cffc3c8
enh3 <xkjelx@gmail.com>
2023-08-13 15:49:15 -0400

    2023-08-11

--- tests/test.py
+++ tests/test.py
@@ -1,34 +1,10 @@
 from pyo import *
-
-pa_list_devices()
-pm_list_devices()
-s = Server()
-s.setOutputDevice(1)
-s.setMidiOutputDevice(98)
-s.setMidiInputDevice(0)
-s.boot()
-
-m = Midictl(ctlnumber=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], minscale=0, maxscale=127, channel=0)
-pp = Print(m, method=1, message="Audio stream value")
-
-def mStateChanges(ctl, chan):
-    global m
-    m.setCtlNumber(ctl) 
-    #print('chan: ', chan)
-    #print('ctl: ', ctl)
-    #print('val: ', int(m.get()))
-    if chan == 6 and val == 20:   
-        #1e Élégie
-        if ctl == 1: 
-            print('Glissandi') 
-        #2e Élégie
-        elif ctl == 2:
-            print('Enveloppe dynamique')
-
-mScan = CtlScan2(mStateChanges, toprint=False)
-
-s.amp = 0.05
-
+s = Server().boot()
 s.start()
-
+m = Midictl(ctlnumber=[3, 4, 5], minscale=0, maxscale=127)
+pp = Print(m, method=1, message="Audio stream value")
+#p = Port(m, .02)
+#a = Sine(freq=p, mul=.3).out()
+#a1 = Sine(freq=p*1.25, mul=.3).out()
+#a2 = Sine(freq=p*1.5, mul=.3).out()
 s.gui(locals())
